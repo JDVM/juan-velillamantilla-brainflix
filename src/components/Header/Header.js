34faad33273621ef4efa0950446c4b28ed97986e
Juan Velilla <juandvelillamantilla@gmail.com>
2023-07-27 22:52:28 -0400

    deleted button componet based on to much granularity. coded into header component directly. fixed spacing on input bar.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,28 @@
 import Logo from "../Assets/Logo/BrainFlix-logo.svg";
-import Button from "../Button/Button";
-import './Header.scss';
-import UploadIcon from '../Assets/Icons/upload.svg'
-import Input from '../Inputs/Input'
-
+import "./Header.scss";
+import UploadIcon from "../Assets/Icons/upload.svg";
+import Search from "../Assets/Icons/search.svg"
 
 function Header() {
-  const uploadIcon = <img src={UploadIcon}/>
+  const uploadIcon = UploadIcon;
   return (
     <header className="header">
       <img src={Logo} className="header__logo" alt="Logo for BrainFlix" />
       <form className="header__form">
         <div className="header__photo"></div>
-        
-        <Input />
-       <Button UploadIcon={uploadIcon} UploadString="UPLOAD"/>
+        <label className="header__label" hmtlfor="Name">
+          <input
+            type="text"
+            name="nameInput"
+            id="Name"
+            className="header__input"
+            placeholder="Search"
+          />
+        </label>
+        <button type="submit" className="header__button">
+          <img src={uploadIcon} className="header__icon" alt="" />
+          <div className="header__text">UPLOAD</div>
+        </button>
       </form>
     </header>
   );
