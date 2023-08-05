21aa0a1ee38eee59d6604959bb02c4d9c70c39c2
Juan Velilla <juanvelilla@Juans-MacBook-Pro.local>
2023-08-05 18:43:42 -0400

    added axios calls for current video and comment functions. redoing comment in correct branch

--- src/components/NextVideo/NextVideo.js
+++ src/components/NextVideo/NextVideo.js
@@ -1,12 +1,11 @@
 import NextVideoEl from "../NextVideoEl/NextVideoEl";
 import "./NextVideo.scss";
 
-function NextVideo(props) {
+function NextVideo({filteredVid}) {
   return (
     <div className="nextvid">
       <span className="nextvid__title">NEXT VIDEOS</span>
-      {/* <NextVideoEl  videos={props.videos} updateVidId={props.updateVidId} vidId={props.vidId}/> */}
-      <NextVideoEl />
+      <NextVideoEl filteredVid={filteredVid} />
     </div>
   );
 }
